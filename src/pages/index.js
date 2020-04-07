96e548abd5e144c29fd17b9428edce54346d2107
Vikram Nadig <Vikram.Nadig@comparisontech.com>
2020-04-07 09:28:15 +0100

    remove unused references

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
-import L from 'leaflet';
-import { Marker } from 'react-leaflet';
-
-import { promiseToFlyTo, getCurrentLocation } from 'lib/map';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
 
-import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
 
 const LOCATION = {
   lat: 38.9072,
@@ -17,27 +12,10 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
 
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
 
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
 
 const IndexPage = () => {
-  const markerRef = useRef();
 
   /**
    * mapEffect
@@ -45,7 +23,7 @@ const IndexPage = () => {
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement: map } = {}) {}
+  async function mapEffect() {}
 
   const mapSettings = {
     center: CENTER,
